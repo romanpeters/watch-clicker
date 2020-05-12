e2552b176bafc709ba646d39a8d295703ed1a87a
Roman Peters <github@romanpeters.nl>
2020-05-12 19:26:39 +0200

    Initial Commit

---
+++ Counter WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Counter WatchKit Extension
+//
+//  Created by Roman Peters on 12/05/2020.
+//  Copyright © 2020 Roman Peters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Counter WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Counter WatchKit Extension
+//
+//  Created by Roman Peters on 12/05/2020.
+//  Copyright © 2020 Roman Peters. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
